c857f0234992d37ecb9560040b37be0dfd2b7f66
HiroshiOHSUGA <hiroshi.ohsuga@eure.jp>
2022-06-20 10:15:10 +0900

    Making it module

--- 05/bloom-filter.ts
+++ 05/bloom-filter.ts
@@ -4,26 +4,36 @@ import { hash, slice, toInt, mod } from "./libs.ts";
 const HASH_COUNT = 3;
 const BITMAP_SCALE = 10;
 
-const reader = await readLine("./wordlist.txt");
-const indexList: number[][] = [];
-for await (let line of reader) {
-  const h = hash(line);
-  const separated = slice(h, HASH_COUNT);
-  indexList.push(separated.map(toInt));
-}
+export const getBloomFilter = async (
+  hashCount: number = HASH_COUNT,
+  bitmapScale: number = BITMAP_SCALE,
+  textBookPath: string = "./wordlist.txt"
+) => {
+  const reader = await readLine(textBookPath);
+  const indexList: number[][] = [];
+  for await (let line of reader) {
+    const h = hash(line);
+    const separated = slice(h, hashCount);
+    indexList.push(separated.map(toInt));
+  }
 
-const BITMAP_SIZE = indexList.length * BITMAP_SCALE;
+  const bitmapSize = indexList.length * bitmapScale;
 
-const bloomMap = indexList.reduce((result, indexes) => {
-  indexes.forEach((index) => {
-    result[mod(result.length)(index)] = true;
+  const bloomMap = indexList.reduce((result, indexes) => {
+    indexes.forEach((index) => {
+      result[mod(result.length)(index)] = true;
+    });
+    return result;
+  }, new Array(bitmapSize));
+  console.log({
+    words: indexList.length,
+    hashPerWord: hashCount,
+    bitmapScale,
+    bitmapSize,
+    usedBit: bloomMap.filter(() => true).length,
   });
-  return result;
-}, new Array(BITMAP_SIZE));
-console.log({
-  words: indexList.length,
-  hashPerWord: HASH_COUNT,
-  BITMAP_SCALE,
-  BITMAP_SIZE,
-  usedBit: bloomMap.filter(() => true).length,
-});
+  return (word: string): boolean => {
+    const hashes = slice(hash(word), hashCount).map(toInt);
+    return hashes.every((h) => bloomMap[h] === true);
+  };
+};
