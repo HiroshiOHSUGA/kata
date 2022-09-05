3a8379accbee2bbfced7548af018a2d528ab7c0a
HiroshiOHSUGA <hiroshi.ohsuga@eure.jp>
2022-09-05 10:31:15 +0900

    Fix syntax

--- 08/expandable/index.ts
+++ 08/expandable/index.ts
@@ -2,9 +2,9 @@ import { findAnswers } from "./libs/findAnswers.ts";
 import { loadWordDB } from "./libs/loadWordDB.ts";
 import type { WordDB } from "./types.d.ts";
 import { parse } from "https://deno.land/std@0.154.0/flags/mod.ts";
-const cliParams = parse(Deno.args);
-const TEXTBOOK_PATH = cliParams.file ?? "./data/wordlist.txt";
-const TARGET_WORD_LENGTH = cliParams.length ?? 6;
+const { file = "./data/wordlist.txt", length = 6 } = parse(Deno.args);
+const TEXTBOOK_PATH = file;
+const TARGET_WORD_LENGTH = length;
 
 console.time("loadWordDB");
 const wordDB: WordDB = await loadWordDB(TEXTBOOK_PATH, TARGET_WORD_LENGTH);
